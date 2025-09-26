d0ac47cfd33714e3b4c564d4594c356c1af37511
Bernhard Koschicek-Krombholz <b.koschicek@gmail.com>
2025-09-26 10:26:06 +0200

    added style in css

--- static/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const btn = document.getElementById("clickBtn");
-    btn.addEventListener("click", () => {
-        alert("Button clicked!");
-    });
-});
